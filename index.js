86afd464f3b5b4601d8ee4d3c273356a9704dfdc
toxidity-18 <desavigatunganga@gmail.com>
2025-03-06 10:15:52 +0300

    learnt how to do uppercase

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Write your solution in this file!
+// 6 |March|2025 
+
+var customerName = 'bob'
+function upperCaseCustomerName(){
+ customerName = customerName.toUpperCase()
+}
+function setBestCustomer(){
+ bestCustomer = 'not bob'
+}
+function overwriteBestCustomer(){
+ bestCustomer = 'maybe bob'
+}
+const leastFavoriteCustomer = 'me'
+function changeLeastFavoriteCustomer(){
+  leastFavoriteCustomer = 'him'
+}
+
